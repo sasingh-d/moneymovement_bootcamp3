8144fc7ae0bea01a6bbaefd7817cc709bf2938ba
Sarika <sarika.d.singh@accenture.com>
2025-01-09 14:06:45 -0600

    Add Topic controller

--- SpringBootApp2/src/main/java/com/myprc/demo/controller/HelloController.java
+++ SpringBootApp2/src/main/java/com/myprc/demo/controller/HelloController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
+
 	@RequestMapping("/hello")
 	public String sayHi() {
 		return "Hi";
 	}
-
 }
